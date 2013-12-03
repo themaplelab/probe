676931cfa68aa837af9e0e6361359378fa482214
karim <karim@e628a048-d915-0410-9aa0-f7087ab87138>
2013-12-03 22:24:45 +0000

    using generics data structures, flushable writer, unified tag names

--- code/probe/src/probe/TextReader.java
+++ code/probe/src/probe/TextReader.java
@@ -1,56 +1,60 @@
 package probe;
-import java.util.*;
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 /** Reads a call graph from a text file. */
 public class TextReader {
-    /** Read a call graph from a text file. */
-    public CallGraph readCallGraph( InputStream file ) throws IOException {
+	/** Read a call graph from a text file. */
+	public CallGraph readCallGraph(InputStream file) throws IOException {
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(file));
-        CallGraph ret = new CallGraph();
+		BufferedReader in = new BufferedReader(new InputStreamReader(file));
+		CallGraph ret = new CallGraph();
 
-        while(true) {
-            String line = in.readLine();
-            if(line == null) break;
-            if(line.equals("CLASS")) {
-                String id = in.readLine();
-                String pkg = in.readLine();
-                String name = in.readLine();
-                ProbeClass cls = ObjectManager.v().getClass(pkg, name);
-                nodeToClass.put(id, cls);
-            } else if(line.equals("METHOD")) {
-                String id = in.readLine();
-                String name = in.readLine();
-                String signature = in.readLine();
-                String cls = in.readLine();
-                ProbeMethod m = ObjectManager.v().getMethod(
-                        (ProbeClass) nodeToClass.get(cls),
-                        name,
-                        signature
-                );
-                nodeToMethod.put(id, m);
-            } else if(line.equals("ENTRYPOINT")) {
-                String id = in.readLine();
-                ret.entryPoints().add( (ProbeMethod) nodeToMethod.get(id) );
-            } else if(line.equals("CALLEDGE")) {
-                String src = in.readLine();
-                String dst = in.readLine();
-                String context = in.readLine();
-                ret.edges().add(new CallEdge(
-                        (ProbeMethod) nodeToMethod.get(src),
-                        (ProbeMethod) nodeToMethod.get(dst)
-                ));
-            } else {
-                throw new RuntimeException("Unexpected line: "+line);
-            }
-        }
+		while (true) {
+			String line = in.readLine();
+			if (line == null)
+				break;
 
-        return ret;
-    }
+			if (line.equals(Util.ClassTag)) {
+				String id = in.readLine();
+				String pkg = in.readLine();
+				String name = in.readLine();
 
-    /* End of public methods. */
+				ProbeClass cls = ObjectManager.v().getClass(pkg, name);
+				nodeToClass.put(id, cls);
+			} else if (line.equals(Util.MethodTag)) {
+				String id = in.readLine();
+				String name = in.readLine();
+				String signature = in.readLine();
+				String cls = in.readLine();
 
-    private Map nodeToClass = new HashMap();
-    private Map nodeToMethod = new HashMap();
+				ProbeMethod m = ObjectManager.v().getMethod(nodeToClass.get(cls), name, signature);
+				nodeToMethod.put(id, m);
+			} else if (line.equals(Util.EntrypointTag)) {
+				String id = in.readLine();
+
+				ret.entryPoints().add(nodeToMethod.get(id));
+			} else if (line.equals(Util.EdgeTag)) {
+				String src = in.readLine();
+				String dst = in.readLine();
+				String weight = in.readLine();
+
+				ret.edges().add(new CallEdge(nodeToMethod.get(src), nodeToMethod.get(dst), Double.parseDouble(weight)));
+			} else {
+				throw new RuntimeException("Unexpected line: " + line);
+			}
+		}
+
+		return ret;
+	}
+
+	/* End of public methods. */
+
+	private Map<String, ProbeClass> nodeToClass = new HashMap<String, ProbeClass>();
+	private Map<String, ProbeMethod> nodeToMethod = new HashMap<String, ProbeMethod>();
 }
